61b88648c55ed59ffca676e8037b48228403ef7f
mmiguez1 <mmiguez1@tulane.edu>
2022-07-12 02:41:06 -0500

    update info

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,128 +1,115 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Icon } from '@components/icons';
+import {
+  IconGithub,
+  IconLinkedin,
+  IconCodepen,
+  IconInstagram,
+  IconTwitter,
+  IconStar,
+  IconFork,
+} from '@components/icons';
 import { socialMedia } from '@config';
+import styled from 'styled-components';
+import { theme, mixins, media } from '@styles';
+const { colors, fontSizes, fonts } = theme;
 
-const StyledFooter = styled.footer`
-  ${({ theme }) => theme.mixins.flexCenter};
+const FooterContainer = styled.footer`
+  ${mixins.flexCenter};
   flex-direction: column;
+  padding: 15px;
+  background-color: ${colors.darkNavy};
+  color: ${colors.slate};
+  text-align: center;
   height: auto;
   min-height: 70px;
-  padding: 15px;
-  text-align: center;
 `;
-
-const StyledSocialLinks = styled.div`
+const SocialContainer = styled.div`
+  color: ${colors.lightSlate};
+  width: 100%;
+  max-width: 270px;
+  margin: 0 auto 10px;
   display: none;
-  @media (max-width: 768px) {
-    display: block;
-    width: 100%;
-    max-width: 270px;
-    margin: 0 auto 10px;
-    color: var(--light-slate);
-  }
-  ul {
-    ${({ theme }) => theme.mixins.flexBetween};
-    padding: 0;
-    margin: 0;
-    list-style: none;
-    a {
-      padding: 10px;
-      svg {
-        width: 20px;
-        height: 20px;
-      }
-    }
+  ${media.tablet`display: block;`};
+`;
+const SocialItemList = styled.ul`
+  ${mixins.flexBetween};
+`;
+const SocialLink = styled.a`
+  padding: 10px;
+  svg {
+    width: 20px;
+    height: 20px;
   }
 `;
-
-const StyledCredit = styled.div`
-  color: var(--light-slate);
-  font-family: var(--font-mono);
-  font-size: var(--fz-xxs);
+const Copy = styled.div`
+  margin: 10px 0;
+  font-family: ${fonts.SFMono};
+  font-size: ${fontSizes.xsmall};
   line-height: 1;
-  a {
-    padding: 10px;
+`;
+const GithubLink = styled.a`
+  color: ${colors.slate};
+`;
+const GithubInfo = styled.div`
+  margin-top: 10px;
+  & > span {
+    display: inline-flex;
+    align-items: center;
+    margin: 0 7px;
   }
-  .github-stats {
-    margin-top: 10px;
-    & > span {
-      display: inline-flex;
-      align-items: center;
-      margin: 0 7px;
-    }
-    svg {
-      display: inline-block;
-      margin-right: 5px;
-      width: 14px;
-      height: 14px;
-    }
+  svg {
+    display: inline-block;
+    height: 15px;
+    width: auto;
+    margin-right: 5px;
   }
 `;
 
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
-  return (
-    <StyledFooter>
-      <StyledSocialLinks>
-        <ul>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
-        </ul>
-      </StyledSocialLinks>
-
-      <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
-      </StyledCredit>
-    </StyledFooter>
-  );
-};
-
+const Footer = ({ githubInfo }) => (
+  <FooterContainer>
+    <SocialContainer>
+      <SocialItemList>
+        {socialMedia &&
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <SocialLink
+                href={url}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label={name}>
+                {name === 'Github' ? (
+                  <IconGithub />
+                ) : name === 'Linkedin' ? (
+                  <IconLinkedin />
+                ) : name === 'Codepen' ? (
+                  <IconCodepen />
+                ) : name === 'Instagram' ? (
+                  <IconInstagram />
+                ) : name === 'Twitter' ? (
+                  <IconTwitter />
+                ) : (
+                  <IconGithub />
+                )}
+              </SocialLink>
+            </li>
+          ))}
+      </SocialItemList>
+    </SocialContainer>
+    <Copy>
+      <GithubLink
+        href="https://github.com/bchiang7"
+        target="_blank"
+        rel="nofollow noopener noreferrer">
+        <div>Original Template Created By Brittany Chiang</div>
+      </GithubLink>
+    </Copy>
+  </FooterContainer>
+);
+  
 Footer.propTypes = {
   githubInfo: PropTypes.object,
 };
-
+   
 export default Footer;
-    
+      
